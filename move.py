f7ac604887e7a05f0d59ed8a8bb00725ec61a9f8
User25311 <106886840+User25311@users.noreply.github.com>
2022-07-23 15:25:58 -0400

    Update move.py

--- move.py
+++ move.py
@@ -1,5 +1,5 @@
 import pygame 
-
+from random import randint
 pygame.init() ## Used to initalize all the rquired modules of pygame
 screenWidth = 800
 screenHeight = 800
@@ -18,6 +18,11 @@ height = 100 #Dimesions of the object (height)
 ## Starting alien properities
 alien_x = 400 # starting x coordinate
 alien_y =  100# Starting y coordinate
+#Starting speed of alien
+alien_speed_x = 50
+alien_speed_y = 50
+# Creating variable for alien starting direction
+direction = 1
 
 clock = pygame.time.Clock() # Used to control the frames per second and thus the game speed 
 run = True #Indiciate that pygame is running and 'while' loop is used
@@ -59,8 +64,7 @@ class alien(object):
         window.blit(ufo, (self.x, self.y))
 
 
-    def draw2(self, window):
-        window.blit(ufo2, (self.x, self.y))
+   
 
 
     #  1. Create a function that draws the aliens in a random position
@@ -87,7 +91,7 @@ def reDrawGameWindow():
     window.blit(space, (0,0))
     p1.draw(window)
     bot.draw(window)
-    bot1.draw2(window)
+    # bot1.draw2(window)
     for bullet in bullets:
         bullet.draw(window)
            
@@ -101,20 +105,46 @@ def reDrawGameWindow():
 
 p1 = rocket(x, y, width, height)
 bot = alien(alien_x, alien_y, width, height)
-bot1 = alien(100, 200, width, height)
+# bot1 = alien(100, 200, width, height)
 ## Store all our projectile objects in a list
 bullets = [] 
+clock.tick(60)
 while run:
+
+   
+
+   
+
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run = False
-
+ 
         for bullet in bullets:
             if bullet.y < 800 and bullet.y > 0:
                 bullet.y += bullet.vel
             else:
                 bullets.pop(bullets.index(bullet)) # this removes the bullet if its off the screen
+         
+        # Responsible for random alien movements:
+        if bot.x <= 20 or bot.x + bot.width >= screenWidth:
+            direction *= -1
+            alien_speed_x = randint(10, 30) * direction
+            alien_speed_y = randint(10, 30) * direction
+
+        # Top and Bottom screen boundary 
+        if bot.y <= 20 or bot.height >= screenHeight:
+            direction *= -1
+            alien_speed_x = randint(15, 40) *direction
+            alien_speed_y = randint(15, 40) *direction
+
+        
+        
+        bot.x += alien_speed_x
+        bot.y += alien_speed_y
+
+
+
             
         keys = pygame.key.get_pressed()
         # 1. Add Up, remember to stop moving up once we have reached the top
